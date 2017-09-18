36d0771f3b427770a24f0c92ff2683554a7755fb
CaptainJohnyAppleSeed <fitzpats@dickinson.edu>
2017-09-18 14:25:10 -0400

    TTBoard.java row2Winner method fixed.

--- TTTBoard.java
+++ TTTBoard.java
@@ -75,7 +75,13 @@ public class TTTBoard {
 	 * @return 'x' if x wins, 'o' if o wins, and null (i.e. '\0') otherwise.
 	 */
 	public char row2winner() {
-		return '\0';
+		if(board[2][0] == 'x' && board[2][1] == 'x' && board[2][2] == 'x'){
+			return 'x';
+		} else if(board[2][0] == 'o' && board[2][1] == 'o' && board[2][2] == 'o'){
+			return 'o';
+		} else{
+			return '\0';
+		}
 	}
 
 	/**
